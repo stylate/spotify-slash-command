758e1a5dad56fa94ccae21f961238f6900259a83
Alan Nguyen <nguyenalan@berkeley.edu>
2018-12-14 15:39:29 -0800

    finished api implementation

--- api/resolver.js
+++ api/resolver.js
@@ -4,10 +4,8 @@ var createTemplate = require('../utils/template.js').resolver;
 // in-email representation
 module.exports = async function (req, res) {
     var term = req.query.text.trim();
-    console.log("term: ", term)
     var spotify_api = await spotify.spotify_auth();
     var response = await spotify.get_track(term, spotify_api);
-    console.log("album artwork: ", response.body.album.images[0].url);
     res.json({
         body: createTemplate(response.body)
     });
